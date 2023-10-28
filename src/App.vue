f892f40a4d292d6fa9c136ab2d7da565b2c90009
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-28 11:54:37 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="app">
+  <div class="header container h-100 p-5">
+    <h1 class="mb-5">Weather App</h1>
+    <div class="d-flex justify-content-center h-100">
+      <div class="searchbar w-50 mx-2">
+        <input type="text" class="input form-control" v-model="city" placeholder="Enter a city">
+      </div>
+      <button class="btn-search btn btn-primary">Search	<i class="fa-solid fa-magnifying-glass"></i> </button>
+    </div>
   </div>
+  <br>
+  <weather></weather>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import weather from './components/App-Weather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    weather,
+  },
+  data(){
+    return{
+      city : '',
+      showWeather: false,
+    }
+  },
+  methods :{
+    async searchWeather (){
+      this.showWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
+  background: #121212 !important;
+}
+.header{
+  background:#212730 ;
+  border-radius: 20px;
+  color: #fff;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow',sans-serif;
+  margin-top: 5rem;
+}
+.btn-search{
+  background-image:linear-gradient(to right, cyan,magenta ) ;
 }
 </style>
